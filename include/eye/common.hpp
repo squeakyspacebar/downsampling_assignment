87b379b93fc0f298137af34c34a9b60bd59ae1b6
Andy Lo <squeakyspacebar@gmail.com>
2017-09-21 19:33:13 -0500

    Implemented parallelization of mode computations.

--- include/eye/common.hpp
+++ include/eye/common.hpp
@@ -10,6 +10,6 @@ namespace eye {
     typedef std::map<image_data_t, std::size_t> mode_map_t;
     typedef std::pair<mode_map_t, image_data_t> mode_pair_t;
     typedef andres::Marray<unsigned int> image_array_t;
-    typedef andres::Marray<mode_map_t> mode_array_t;
+    typedef std::vector<mode_map_t> mode_array_t;
 }
 #endif
